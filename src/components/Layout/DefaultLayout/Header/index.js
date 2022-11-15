b6114faa7554813391c5fe8ffd0841cbaf5bc4ee
leanbuu <88716598+leanbuu@users.noreply.github.com>
2022-11-15 14:14:37 +0700

    them search

--- src/components/Layout/DefaultLayout/Header/index.js
+++ src/components/Layout/DefaultLayout/Header/index.js
@@ -1,13 +1,14 @@
 
 import classNames from "classnames/bind";
 import styles from '../Header/Header.module.scss';
+import Search from "../../components/Search";
 
 const cx = classNames.bind(styles);
 
 
 function Header() {
     return <div className={cx('header')}>
-
+       <Search />
     </div>;
 }
 
---
+++ src/components/Layout/components/Search/index.js
@@ -0,0 +1,24 @@
+
+import classNames from "classnames/bind";
+import styles from '../Search/Search.module.scss';
+import { SearchIcon } from "../Icons";
+
+const cx = classNames.bind(styles);
+
+
+function Search() {
+    return ( 
+        <div className={cx('search')}>
+            <div className={cx('inputt')}>
+            <input className={cx('inputt-text')}
+                placeholder="Tìm kiếm bài hát, nghệ sĩ, lời bài hát..."
+            ></input>
+            </div>
+            <button className={cx('search-btn')}>
+                <SearchIcon></SearchIcon>
+            </button>
+        </div>
+     );
+}
+
+export default Search;
