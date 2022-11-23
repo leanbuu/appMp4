a4f57561611f20db7f53b882ceb1d83d96e03dbb
leanbuu <88716598+leanbuu@users.noreply.github.com>
2022-11-23 17:54:15 +0700

    hoàn thành header giao diện

--- src/components/Layout/DefaultLayout/Header/index.js
+++ src/components/Layout/DefaultLayout/Header/index.js
@@ -4,11 +4,17 @@ import styles from '../Header/Header.module.scss';
 import Search from "../../components/Search";
 import ItemSetting from "../../components/Itemsetting";
 import Avarta from "../../components/Avarta";
+import Backbtn from "../../components/Backbtn";
+
+import Tippy from '@tippyjs/react';
+import 'tippy.js/dist/tippy.css';
 
 import icon1 from '../../../../assets/Icon-chude.png'
 import icon2 from '../../../../assets/Icon-nangcap.png'
 import icon3 from '../../../../assets/Icon-tailen.png'
 import icon4 from '../../../../assets/Icon-caidat.png'
+import backri from "../../../../assets/Icon-backright.png"
+import backle from "../../../../assets/Icon-backleft.png"
 
 
 
@@ -17,12 +23,31 @@ const cx = classNames.bind(styles);
 
 function Header() {
     return <div className={cx('header')}>
+       <Backbtn props={backri}/>
+       <Backbtn props={backle}/>
        <Search />
+
        <div className={cx('menu-item')}>
-       <ItemSetting props={icon1} />
-       <ItemSetting props={icon2} />
-       <ItemSetting props={icon3} />
-       <ItemSetting props={icon4} />
+       <Tippy delay={[0, 10]} content={'Chủ đề'} placement='bottom'>
+              <div className={cx('btn-item')}>
+              <ItemSetting props={icon1} />
+              </div>
+        </Tippy>
+        <Tippy delay={[0, 10]} content={'Nâng cấp VIP'} placement='bottom'>
+              <div className={cx('btn-item')}>
+              <ItemSetting props={icon2} />
+              </div>
+        </Tippy>
+        <Tippy delay={[0, 10]} content={'Tải lên'} placement='bottom'>
+              <div className={cx('btn-item')}>
+              <ItemSetting props={icon3} />
+              </div>
+        </Tippy>
+        <Tippy delay={[0, 10]} content={'Cài đặt'} placement='bottom'>
+              <div className={cx('btn-item')}>
+              <ItemSetting props={icon4} />
+              </div>
+        </Tippy>
        <Avarta />
        </div>
     </div>;
