215a53e2c9b0dfc564e0c11994dacc41ca1e9250
leanbuu <88716598+leanbuu@users.noreply.github.com>
2022-12-06 02:32:03 +0700

    mobile

--- src/components/Layout/components/DefaultMobile/MobileHeader/MobileHeader.js
+++ src/components/Layout/components/DefaultMobile/MobileHeader/MobileHeader.js
@@ -1,11 +1,14 @@
 import classNames from "classnames/bind";
 import styles from '../MobileHeader/MobileHeader.module.scss'
 import icon1 from '../../../../../assets/Icon-btnmobile.jpg'
+import icon2 from '../../../../../assets/Icon-close.jpg'
 import { Link } from "react-router-dom";
 import logo from '../../../../../assets/Icon-logomobi.jpg'
 import imgsearch from '../../../../../assets/Icon-searchmobile.jpg'
 import avarta from '../../../../../assets/anhThii.jpg'
 import MenuInner from "../../MenuInner/MenuInner";
+import { useState } from "react";
+
 
 
 const cx = classNames.bind(styles)
@@ -25,12 +28,34 @@ const INNER = [
 ]
 
 
+
 function MobileHeader() {
+   const [dis, setDis] = useState('none')
+   const [con, setCon] = useState('100%')
+   function handleShow() {
+      setDis('block');
+      setCon('0%')
+   }
+   function handleClose() {
+      setDis('none');
+      setCon('100%')
+   }
     return ( 
       <div>
+        <div className={cx('navi')} style={{display: dis }}>
+         <div className={cx('navi-con')}  style={{left: con}}>
+         <div className={cx('usernavi')}>
+            <img style={{width: '60px', height: '60px', borderRadius: '50%'}} src={avarta} alt=''></img>
+            <button className={cx('btn-close')}  onClick={handleClose} >
+               <img src={icon2} alt=''></img>
+            </button>
+            </div>
+         </div>
+         </div>
         <div className={cx('header')}>
+       
            <div className={cx('btnmobile')}>
-             <button className={cx('btnheader')}>
+             <button onClick={handleShow} className={cx('btnheader')}>
                 <img className={cx('icon')} src={icon1} alt=''></img>
              </button>
            </div>
